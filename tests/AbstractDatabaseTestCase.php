a62ef3c26fa6ad33e0d7254d84f536baba741861
Andrzej Kostrzewa <andrkos287@student.polsl.pl>
2015-08-24 22:16:53 +0200

    php-cs-fixer fix

--- tests/AbstractDatabaseTestCase.php
+++ tests/AbstractDatabaseTestCase.php
@@ -1,14 +1,14 @@
 <?php
 abstract class AbstractDatabaseTestCase extends PHPUnit_Extensions_Database_TestCase
 {
-    public final function getConnection()
+    final public function getConnection()
     {
         $pdo = new PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWORD']);
         return $this->createDefaultDBConnection($pdo, $GLOBALS['DB_DBNAME']);
     }
 
     protected function getTearDownOperation()
-	{
-	    return \PHPUnit_Extensions_Database_Operation_Factory::TRUNCATE();
-	}
+    {
+        return \PHPUnit_Extensions_Database_Operation_Factory::TRUNCATE();
+    }
 }
